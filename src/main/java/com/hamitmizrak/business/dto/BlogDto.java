acf9dfe016350f87f37924876b89a492b93fbd5a
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-20 16:10:27 +0300

    BlogDto Validation

--- src/main/java/com/hamitmizrak/business/dto/BlogDto.java
+++ src/main/java/com/hamitmizrak/business/dto/BlogDto.java
@@ -1,11 +1,12 @@
 package com.hamitmizrak.business.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-
 import java.io.Serializable;
 import java.util.Date;
 
@@ -15,13 +16,23 @@ import java.util.Date;
 @NoArgsConstructor
 @Log4j2
 @Builder
-
+// Validation
 public class BlogDto implements Serializable {
 
     // serileştirme
     public static final Long serialVersionUID=1L;
 
+    // HEADER
+    @NotEmpty(message = "Başlık alanı boş geçemezsiniz")
+    @Size(min=10,message = "Başlık en az 10 karakterden küçük olamaz.")
     private String header;
+
+    // CONTENT
+    @NotEmpty(message = "İçerik boş geçilemez")
+    @Size(min = 10,message = "İçerik en az 10 karakter olmalı, küçük giremezsiniz")
     private String content;
-    private Date systemDate;
-}
+
+    // DATE Builder => default olarak ayarlıyor
+    @Builder.Default
+    private Date systemDate=new Date(System.currentTimeMillis());
+} //end class
