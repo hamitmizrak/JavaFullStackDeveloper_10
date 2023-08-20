0e2c42ed3434bfcf9d733978d56fc46f9c69b816
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-20 17:40:03 +0300

    Blog ve  Kategori ValidationMessages.properties

--- src/main/java/com/hamitmizrak/business/dto/BlogDto.java
+++ src/main/java/com/hamitmizrak/business/dto/BlogDto.java
@@ -32,5 +32,4 @@ public class BlogDto extends AuditingAwareBaseDto implements Serializable {
     @NotEmpty(message = "{blog.content.validation.constraints.NotNull.message}")
     @Size(min = 10,message = "{blog.content.least.validation.constraints.NotNull.message}")
     private String content;
-
 } //end class
