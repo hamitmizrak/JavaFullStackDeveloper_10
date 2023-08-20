7d68a2a27526291982c394de3725d260843c2bb5
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-20 17:04:36 +0300

    CategoryDto

--- src/main/java/com/hamitmizrak/business/dto/CategoryDto.java
+++ src/main/java/com/hamitmizrak/business/dto/CategoryDto.java
@@ -1,5 +1,19 @@
 package com.hamitmizrak.business.dto;
 
+import com.hamitmizrak.audit.AuditingAwareBaseDto;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.io.Serializable;
+
 // CategoryDto(1) - BlogDto(N)
-public class CategoryDto {
+public class CategoryDto  extends AuditingAwareBaseDto implements Serializable {
+
+    // serileştirme
+    public static final Long serialVersionUID=1L;
+
+    // HEADER
+    @NotEmpty(message = "Kategory alanı boş geçemezsiniz")
+    @Size(min=10,message = "Kategory en az 10 karakterden küçük olamaz.")
+    private String categoryName;
 }
