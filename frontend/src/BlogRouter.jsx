e89839db5a7ecfb4230089ebb058ed6fe94be941
Hamit Mızrak <hamitmizrak@gmail.com>
2023-09-09 17:48:01 +0300

    Header, Main, Category

--- frontend/src/BlogRouter.jsx
+++ frontend/src/BlogRouter.jsx
@@ -1,33 +1,67 @@
 // rcc
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import {Navigate, Route, Routes} from "react-router-dom";
 
-// CLASS
+// Header, Main, Footer
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import Main from "./component/Main";
+
+// Category
+import CategoryList from "./component/category/CategoryList";
+import CategoryCreate from "./component/category/CategoryCreate";
+import CategoryUpdate from "./component/category/CategoryUpdate";
+
+// i18n
+import { withTranslation } from 'react-i18next';
+import CategoryView from "./component/category/CategoryView";
+
 // Class Component Template
-export default class BlogRouter extends Component {
+ class BlogRouter extends Component {
 
-  // Ekranda görünen
-  static displayName="Blog Router"
+    // Ekranda görünen
+    static displayName = "Blog Router"
 
-  // CONSTRUCTOR
-  constructor(props) {
-    super(props);
+    // CONSTRUCTOR
+    constructor(props) {
+        super(props);
 
-    // STATE
-    this.state={}
+        // STATE
+        this.state = {}
 
-    // BIND
-  }
+        // BIND
+    }
 
-  // CDM
+    // CDM
 
-  // RENDER
-  render() {
+    // RENDER
+    render() {
 
-    // RETURN
-    return (
-      <React.Fragment>
-        BlogRouter
-      </React.Fragment>
-    ) //end return
-  } // // end render
+        // RETURN
+        return (
+            <React.Fragment>
+                {/*Header*/}
+                <Header logo="fa-solid fa-jet-fighter-up"></Header>
+
+                <Routes>
+                    {/*Root Path*/}
+                    <Route path={"/"} element={<Main/>} />
+
+                    <Route path={"/category/list"} element={<CategoryList name="Category List"/>}/>
+                    <Route path={"/category/create"} element={<CategoryCreate name="Category Create"/>}/>
+                    <Route path={"/category/view/:id"} element={<CategoryView name="Category View" />} />
+                    <Route path={"/category/update/:id"} element={<CategoryUpdate name="Category Update"/>}/>
+
+                    {/* Bad Request*/}
+                    <Route path={"*"} element={<Navigate to={"/"} />} />
+                </Routes>
+
+                {/*Footer*/}
+                <Footer copy="&copy; Bütün Haklar Saklıdır."></Footer>
+            </React.Fragment>
+        ) //end return
+    } // // end render
 } //end class
+
+// Wrapper Higher Order Component i18n
+export default withTranslation()(BlogRouter);
---
+++ frontend/src/component/Main.jsx
@@ -0,0 +1,14 @@
+//rcc
+import React, {Component} from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                Main
+            </div>
+        );
+    }
+}
+
+export default Main;
